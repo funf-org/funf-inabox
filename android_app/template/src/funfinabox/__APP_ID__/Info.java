e30ae6943706586df6a6ba1b89bf029889af3505
Alan Gardner <alan.gardner@gmail.com>
2012-01-24 10:53:12 -0800

    Added android app template and app directory template.

---
+++ android_app/template/src/funfinabox/__APP_ID__/Info.java
@@ -0,0 +1,15 @@
+package funfinabox.__APP_ID__;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
