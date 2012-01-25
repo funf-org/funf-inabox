f0efccbe7cbb7b06beb0f340d16e88e2f5b09044
Alan Gardner <alan.gardner@gmail.com>
2012-01-25 12:18:02 -0800

    Restructure app generator code and templates.

---
+++ app_generator/genfunfapp.py
@@ -0,0 +1,12 @@
+
+# Copy directory structure template
+## Create ssh key
+## Create encryption password/key
+## Create Funf Config
+## Create App Config (May roll into funf config)
+
+# Create android app
+## Copy template
+## Create inabox.properties
+## ant clean customize release
+## copy signed android app
