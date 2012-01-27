98ee9f29d390962a91046b0011f73c0f8eb3a670
Alan Gardner <alan.gardner@gmail.com>
2012-01-27 09:59:56 -0800

    Updated info activity interface, made email clickable, one click uninstall, and funf in a box logo clickable.

--- app_generator/android_app_template/src/funfinabox/__ID__/Info.java
+++ app_generator/android_app_template/src/funfinabox/__ID__/Info.java
@@ -1,8 +1,15 @@
 package funfinabox.__ID__;
 
-import funfinabox.__ID__.R;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Info extends Activity
 {
@@ -17,5 +24,28 @@ public class Info extends Activity
         if (!LauncherReceiver.isLaunched()) {
         	LauncherReceiver.launch(this);
         }
+        
+        ((TextView)findViewById(R.id.contact_email)).setMovementMethod(LinkMovementMethod.getInstance());
+        
+        ((Button)findViewById(R.id.uninstall_button)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Uri packageURI = Uri.parse("package:" + getPackageName());
+				Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageURI);
+				startActivity(uninstallIntent);
+			}
+		});
+        
+        ((ImageView)findViewById(R.id.logo_button)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String url = "http://www.funf.org/inabox/";  
+				Intent i = new Intent(Intent.ACTION_VIEW);  
+				i.setData(Uri.parse(url));  
+				startActivity(i);  
+			}
+		});
     }
 }
